e7a7406ad556c8376a6cfcd02ce68802ff62d696
huangzq <13402318457@163.com>
2026-01-08 17:47:03 +0800

    feat(data-gui): 添加3D场景交互控制界面
    添加OrbitControls和GUI库实现3D场景交互控制
    - 创建立方体网格并添加颜色和位置控制
    - 添加点光源并支持位置和强度调整
    - 实现相机控制和渲染循环
    - 添加GUI面板用于控制对象属性和触发逻辑

--- packages/data-gui/main.js
+++ packages/data-gui/main.js
@@ -1,4 +1,8 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { GUI } from "three/examples/jsm/libs/lil-gui.module.min.js";
+
+const gui = new GUI();
 
 const scene = new THREE.Scene();
 console.log(
@@ -6,3 +10,72 @@ console.log(
   "font-size:13px; background:#612830; color:#a56c74;",
   scene
 );
+
+const geometry = new THREE.BoxGeometry(100, 100, 100);
+const material = new THREE.MeshLambertMaterial({
+  color: new THREE.Color("orange"),
+});
+const mesh = new THREE.Mesh(geometry, material);
+mesh.position.set(0, 0, 0);
+scene.add(mesh);
+
+const meshFolder = gui.addFolder("Mesh");
+meshFolder.addColor(mesh.material, "color");
+meshFolder.add(mesh.position, "x").step(10);
+meshFolder.add(mesh.position, "y").step(10);
+meshFolder.add(mesh.position, "z").step(10);
+
+const pointLight = new THREE.PointLight(0xffffff, 10000);
+pointLight.position.set(80, 80, 80);
+scene.add(pointLight);
+
+const lightFolder = gui.addFolder("Light");
+lightFolder.add(pointLight.position, "x").step(10);
+lightFolder.add(pointLight.position, "y").step(10);
+lightFolder.add(pointLight.position, "z").step(10);
+lightFolder.add(pointLight, "intensity").step(1000);
+
+const axesHelper = new THREE.AxesHelper(200);
+scene.add(axesHelper);
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+const camera = new THREE.PerspectiveCamera(60, width / height, 1, 1000);
+camera.position.set(200, 200, 200);
+camera.lookAt(0, 0, 0);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(width, height);
+
+function render() {
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
+}
+
+render();
+
+document.body.append(renderer.domElement);
+
+new OrbitControls(camera, renderer.domElement);
+
+const otherFolder = gui.addFolder("Other");
+const obj = {
+  aaa: "天王盖地虎",
+  bbb: false,
+  ccc: 0,
+  ddd: "111",
+  fff: "Bbb",
+  logic: function () {
+    alert("执行一段逻辑!");
+  },
+};
+
+otherFolder.add(obj, "aaa").onChange((value) => {
+  console.log("aaa", value);
+});
+otherFolder.add(obj, "bbb");
+otherFolder.add(obj, "ccc").min(-10).max(10).step(0.5);
+otherFolder.add(obj, "ddd", ["111", "222", "333"]);
+otherFolder.add(obj, "fff", { Aaa: 0, Bbb: 0.1, Ccc: 5 });
+otherFolder.add(obj, "logic");
