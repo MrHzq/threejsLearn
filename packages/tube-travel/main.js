35b5943630118c5954c56e7788e8b4f9b0258a24
huangzq <13402318457@163.com>
2026-01-08 21:54:49 +0800

    feat: 添加3D管道动画效果及交互控制
    实现3D管道模型并添加纹理贴图
    添加相机沿管道路径移动的动画效果
    增加键盘控制功能，可通过方向键控制相机移动速度

--- packages/tube-travel/main.js
+++ packages/tube-travel/main.js
@@ -1,25 +1,37 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import mesh from "./mesh.js";
+import mesh, { tubePoints } from "./mesh.js";
 
 const scene = new THREE.Scene();
 
 scene.add(mesh);
 
-const axesHelper = new THREE.AxesHelper(200);
-scene.add(axesHelper);
+const pointLight = new THREE.PointLight(0xffffff, 200);
+pointLight.position.set(80, 80, 80);
+scene.add(pointLight);
 
 const width = window.innerWidth;
 const height = window.innerHeight;
 
-const camera = new THREE.PerspectiveCamera(60, width / height, 1, 1000);
-camera.position.set(0, 100, 200);
+const camera = new THREE.PerspectiveCamera(60, width / height, 1, 10000);
+camera.position.set(200, 200, 200);
 camera.lookAt(0, 0, 0);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(width, height);
 
+let i = 0;
 function render() {
+  if (i < tubePoints.length) {
+    if (i < 0) i = 0;
+    camera.position.copy(tubePoints[i]);
+    camera.lookAt(tubePoints[i + 1]);
+    // i++;
+  } else i = 0;
+
+  // 水平方向偏移
+  mesh.material.map.offset.y += 0.01 / 10;
+
   renderer.render(scene, camera);
   requestAnimationFrame(render);
 }
@@ -29,3 +41,19 @@ render();
 document.body.append(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === " ") {
+    i = 0;
+  }
+
+  if (e.key === "ArrowUp") {
+    i += 4;
+    if (i >= tubePoints.length) i = 0;
+  }
+
+  if (e.key === "ArrowDown") {
+    i -= 4;
+    if (i < 0) i = 0;
+  }
+});
