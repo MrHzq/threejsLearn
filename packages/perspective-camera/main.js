01bd5004eb9c20e11ca94b41101c4e75414fd593
huangzq <13402318457@163.com>
2026-01-08 18:03:48 +0800

    feat(perspective-camera): 添加透视相机示例和轨道控制器
    实现一个完整的透视相机示例，包括相机参数调整的GUI界面和轨道控制器交互功能

--- packages/perspective-camera/main.js
+++ packages/perspective-camera/main.js
@@ -1,8 +1,49 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GUI } from "three/examples/jsm/libs/lil-gui.module.min.js";
 
 const scene = new THREE.Scene();
-console.log(
-  "%c [ scene ]",
-  "font-size:13px; background:#612830; color:#a56c74;",
-  scene
-);
+
+const axesHelper = new THREE.AxesHelper(200);
+// scene.add(axesHelper);
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+const camera = new THREE.PerspectiveCamera(60, width / height, 1, 1000);
+camera.position.set(200, 200, 200);
+camera.lookAt(0, 0, 0);
+
+const camera2 = new THREE.PerspectiveCamera(20, 16 / 9, 100, 300);
+let cameraHelper = new THREE.CameraHelper(camera2);
+scene.add(cameraHelper);
+
+const gui = new GUI();
+function onChange() {
+  scene.remove(cameraHelper);
+  cameraHelper = new THREE.CameraHelper(camera2);
+  scene.add(cameraHelper);
+}
+gui.add(camera2, "fov", [10, 20, 30, 40, 50, 60]).onChange(onChange);
+gui
+  .add(camera2, "aspect", {
+    "16/9": 16 / 9,
+    "4/3": 4 / 3,
+  })
+  .onChange(onChange);
+gui.add(camera2, "near", 0, 300).onChange(onChange);
+gui.add(camera2, "far", 300, 800).onChange(onChange);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(width, height);
+
+function render() {
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
+}
+
+render();
+
+document.body.append(renderer.domElement);
+
+new OrbitControls(camera, renderer.domElement);
