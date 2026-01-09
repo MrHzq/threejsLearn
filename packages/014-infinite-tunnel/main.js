3aaa82529f7cf4d008b8b97e29f647f79b9ddfdf
huangzq <13402318457@163.com>
2026-01-09 10:42:48 +0800

    feat(014-infinite-tunnel): 添加无限隧道效果并更新相机位置
    添加隧道网格和纹理，实现旋转和颜色变化动画
    更新相机初始位置并添加控制台日志输出相机位置
    在package.json中添加开发脚本

--- packages/014-infinite-tunnel/main.js
+++ packages/014-infinite-tunnel/main.js
@@ -14,13 +14,24 @@ const width = window.innerWidth;
 const height = window.innerHeight;
 
 const camera = new THREE.PerspectiveCamera(60, width / height, 1, 10000);
-camera.position.set(200, 200, 200);
+camera.position.set(0.9, -520, 6.5);
 camera.lookAt(0, 0, 0);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(width, height);
 
+let H = 0;
+const clock = new THREE.Clock();
 function render() {
+  const delta = clock.getDelta();
+
+  H += 0.002;
+  if (H > 1) H = 0;
+  mesh.material.color.setHSL(H, 0.5, 0.5);
+
+  mesh.material.alphaMap.offset.y += 0.5 * delta;
+  mesh.rotation.y += 0.5 * delta;
+
   renderer.render(scene, camera);
   requestAnimationFrame(render);
 }
@@ -30,3 +41,7 @@ render();
 document.body.append(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
+
+controls.addEventListener("change", () => {
+  console.log("[ camera ] >", camera.position);
+});
--- packages/014-infinite-tunnel/mesh.js
+++ packages/014-infinite-tunnel/mesh.js
@@ -1,1 +1,20 @@
-export default {};
+import * as THREE from "three";
+
+const geometry = new THREE.CylinderGeometry(30, 50, 1000, 32, 32, true);
+
+const loader = new THREE.TextureLoader();
+const texture = loader.load("./suidao.webp");
+texture.colorSpace = THREE.SRGBColorSpace;
+texture.wrapS = THREE.RepeatWrapping;
+texture.wrapT = THREE.RepeatWrapping;
+texture.repeat.set(1, 2);
+
+const material = new THREE.MeshBasicMaterial({
+  transparent: true,
+  alphaMap: texture,
+  side: THREE.DoubleSide,
+});
+
+const tunnel = new THREE.Mesh(geometry, material);
+
+export default tunnel;
