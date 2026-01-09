9b0689087f7174ee7ecd2a125ffad6282b213dd3
huangzq <13402318457@163.com>
2026-01-09 10:26:23 +0800

    feat: 添加013-tube-travel和014-infinite-tunnel项目文件
    重构tube-travel项目为013-tube-travel，并新增014-infinite-tunnel项目
    包含HTML、JS、资源文件和package.json配置

---
+++ packages/014-infinite-tunnel/main.js
@@ -0,0 +1,32 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import mesh from "./mesh.js";
+
+const scene = new THREE.Scene();
+
+scene.add(mesh);
+
+const pointLight = new THREE.PointLight(0xffffff, 200);
+pointLight.position.set(80, 80, 80);
+scene.add(pointLight);
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+const camera = new THREE.PerspectiveCamera(60, width / height, 1, 10000);
+camera.position.set(200, 200, 200);
+camera.lookAt(0, 0, 0);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(width, height);
+
+function render() {
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
+}
+
+render();
+
+document.body.append(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
